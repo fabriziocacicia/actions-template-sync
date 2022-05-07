9eaa2a8814192e451a6406e9a197ef3d0d386828
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-05-07 17:51:53 +0200

    Add URL formatting in the PR body text

--- src/sync_template.sh
+++ src/sync_template.sh
@@ -106,7 +106,7 @@ echo "::endgroup::"
 echo "::group::create pull request"
 gh pr create \
   --title "${PR_TITLE}" \
-  --body "This PR will merge the ${SOURCE_REPO_PATH} changes" \
+  --body "This PR will merge the [${SOURCE_REPO_PATH}]((https://github.com${SOURCE_REPO_PATH}/) changes" \
   -B "${UPSTREAM_BRANCH}" \
   -l "${PR_LABELS}"
 echo "::endgroup::"
